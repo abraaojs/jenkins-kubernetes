4fdffcc1ae4bcc3af2b8c7014fac7e39582f1c84
Abraão Silva <abraao.silva@assai.com.br>
2020-02-27 11:45:08 -0300

    add new projetc

--- resources/views/layouts/default/footer.blade.php
+++ resources/views/layouts/default/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="page-footer">
     <div class="container">
         &copy; Copyright {{ date('Y') }}
-        <a href="http://schoolofnet.com" class="grey-text text-lighten-4 right">School of Net</a>
+        <a href="https://github.com/abraaojs" class="grey-text text-lighten-4 right">Abraão Silva</a>
     </div>
 </footer>
